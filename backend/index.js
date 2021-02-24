783c0dfd118ad102baf0c374e5cad11b464f544b
Bryan Yi <bryanyi1206@gmail.com>
2021-02-24 12:47:05 -0800

    deploying to heroku

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use("/api/stripe", require("./routes/paymentRoutes"));
 app.use(errorHandler);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("frontend/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
   });
--- frontend/src/redux/reducers/searchNavReducer.js
+++ frontend/src/redux/reducers/searchNavReducer.js
@@ -4,7 +4,10 @@ export const searchNavReducer = (state = { searchNav: [] }, action) => {
   switch (action.type) {
     case actionTypes.SEARCH_NAVBAR:
       return {
-        searchNav: action.payload.target.value.toLowerCase() || null,
+        searchNav:
+          action.payload.target == undefined
+            ? " "
+            : action.payload.target.value.toLowerCase(),
       };
     case actionTypes.SEARCH_BRAND_NAVBAR:
       return {
