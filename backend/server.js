fa7be3b5c50ffe4238732e5516e4e70d45b704ca
Bryan Yi <bryanyi1206@gmail.com>
2021-02-24 10:09:56 -0800

    first heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,7 @@ app.use(errorHandler);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, ".../frontend", "build", "index.html")
-    );
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
   });
 }
 
