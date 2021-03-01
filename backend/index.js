379d366372307c5eb84e8e83bdc75fbc8b6d968c
Bryan Yi <bryanyi1206@gmail.com>
2021-03-01 09:22:13 -0800

    fixed more responsiveness details

--- backend/index.js
+++ backend/index.js
@@ -18,12 +18,12 @@ app.use("/api/stripe", require("./routes/paymentRoutes"));
 app.use(errorHandler);
 app.use(express.static(path.join(__dirname, "frontend/build")));
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("frontend/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
